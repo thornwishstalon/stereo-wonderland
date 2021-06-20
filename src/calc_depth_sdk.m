5b2513b606b2ed3be38e187480508a0b05c66e07
Fabian Pechstein <thornwishstalon@gmail.com>
2021-06-20 19:47:44 +0200

    add middlebury sdk code, make compatible

---
+++ src/calc_depth_sdk.m
@@ -0,0 +1,27 @@
+function [filled_left, filled_right, cleaned_left, cleaned_right ] = calc_depth_sdk(left_image,right_image, max_disparity)
+%CALC_DEPTH aggregated function to calc depth maps for task 4
+%   Detailed explanation goes here
+
+    
+% fixed parameters!!!!
+window_size = 3;
+filter_neighborhood = 42; 
+filter_smoothing = 0.5;
+
+% calculate cost volume
+[ cost_volume_left, cost_volume_right] = guided_cost_volume( left_image, right_image, max_disparity, window_size, filter_neighborhood, filter_smoothing); 
+[min_l, ind_l] = min(cost_volume_left,[] ,3);
+[min_r, ind_r] = min(cost_volume_right,[] ,3);
+
+
+% consistency check
+[cleaned_left, cleaned_right]  = filter_occlusions(ind_l, ind_r, 0);
+% fill occlusions
+[filled_left, filled_right]  = fill_occlusions(cleaned_left, cleaned_right,  max_disparity, [0 0 ]);
+% smoothing with median filter
+filled_left = medfilt2(filled_left);
+filled_right = medfilt2(filled_right);
+
+
+end
+
